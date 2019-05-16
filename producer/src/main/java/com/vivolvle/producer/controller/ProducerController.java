8d915bb40052fd8d3d29cafd49b5d2f0e8271fea
魏良政 <1067310192@qq.com>
2019-05-16 16:40:38 +0800

    实现实时动态部署更新

--- producer/src/main/java/com/vivolvle/producer/controller/ProducerController.java
+++ producer/src/main/java/com/vivolvle/producer/controller/ProducerController.java
@@ -4,6 +4,7 @@ import com.vivolvle.producer.model.MapTheme;
 import com.vivolvle.producer.service.MapThemeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.List;
  * @Date: 2019/5/14 16:10
  */
 @RestController
+@RefreshScope
 public class ProducerController {
     @Autowired
     private MapThemeService mapThemeService;
