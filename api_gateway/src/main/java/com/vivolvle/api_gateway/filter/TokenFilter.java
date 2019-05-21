920b18832af69330e93e8f8013366b9a0fcfb344
魏良政 <1067310192@qq.com>
2019-05-21 17:30:19 +0800

    filter

---
+++ api_gateway/src/main/java/com/vivolvle/api_gateway/filter/TokenFilter.java
@@ -0,0 +1,35 @@
+package com.vivolvle.api_gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * @Author: weilz
+ * @Date: 2019/5/21 17:24
+ */
+@Component
+public class TokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
