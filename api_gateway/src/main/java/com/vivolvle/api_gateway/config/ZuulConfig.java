b8bf2f831ea8484b6af65357306868a3a2fedcb6
魏良政 <1067310192@qq.com>
2019-05-21 17:05:40 +0800

    屌的一匹，接下来可以更屌

---
+++ api_gateway/src/main/java/com/vivolvle/api_gateway/config/ZuulConfig.java
@@ -0,0 +1,16 @@
+package com.vivolvle.api_gateway.config;
+
+/**
+ * 配置类，实现动态路由，结合cloud的web钩子 最后有待测试
+ * @Author: weilz
+ * @Date: 2019/5/21 17:00
+ */
+//@Component
+//public class ZuulConfig {
+//
+//    @ConfigurationProperties("zuul")
+//    @RefreshScope
+//    public ZuulProperties zuulProperties(){
+//        return new ZuulProperties();
+//    }
+//}
