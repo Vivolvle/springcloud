fc93ebc6d67921edc5b6bded86f6072e4875ee63
魏良政 <1067310192@qq.com>
2019-05-22 18:16:58 +0800

    把拦截器区分为卖家和买家

---
+++ api_gateway/src/main/java/com/vivolvle/api_gateway/filter/AuthBuyerFilter.java
@@ -0,0 +1,61 @@
+package com.vivolvle.api_gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import com.vivolvle.api_gateway.utils.CookieUtil;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * 权限拦截（买家）
+ *
+ * @Author: weilz
+ * @Date: 2019/5/21 17:24
+ */
+@Component
+public class AuthBuyerFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        if ("/user/order/create".equals(request.getRequestURI())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        /**
+         * /order/create 只能买家访问 openId
+         * /order/finish 只能卖家访问 token
+         * /product/list 都能访问
+         */
+        Cookie cookie = CookieUtil.get(request, "openId");
+        if (cookie == null || StringUtils.isEmpty(cookie.getValue())) {
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+        }
+        return null;
+    }
+}
--- api_gateway/src/main/java/com/vivolvle/api_gateway/filter/AuthFilter.java
+++ api_gateway/src/main/java/com/vivolvle/api_gateway/filter/AuthFilter.java
@@ -2,6 +2,7 @@ package com.vivolvle.api_gateway.filter;
 
 /**
  * 权限拦截（区分买家和卖家）
+ *
  * @Author: weilz
  * @Date: 2019/5/21 17:24
  */
@@ -27,10 +28,24 @@ package com.vivolvle.api_gateway.filter;
 //        RequestContext requestContext = RequestContext.getCurrentContext();
 //        HttpServletRequest request = requestContext.getRequest();
 //        /**
-//         * /order/create 只能买家访问
-//         * /order/finish 只能卖家访问
+//         * /order/create 只能买家访问 openId
+//         * /order/finish 只能卖家访问 token
 //         * /product/list 都能访问
 //         */
+//        if ("/user/order/create".equals(request.getRequestURI())) {
+//            Cookie cookie = CookieUtil.get(request, "openId");
+//            if (cookie == null || StringUtils.isEmpty(cookie.getValue())) {
+//                requestContext.setSendZuulResponse(false);
+//                requestContext.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+//            }
+//        }
+//        if(request.getRequestURI().startsWith("/user/order/finish/")){
+//            Cookie cookie = CookieUtil.get(request, "token");
+//            if (cookie == null || StringUtils.isEmpty(cookie.getValue())) {
+//                requestContext.setSendZuulResponse(false);
+//                requestContext.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+//            }
+//        }
 //        return null;
 //    }
 //}
